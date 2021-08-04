55a1ab80acf309ee574b44ead6a8ea4891c290f5
Jhon Rey <jhonreymendiola@gmail.com>
2021-08-05 00:47:55 +0800

    fixed images

--- pages/carousel.js
+++ pages/carousel.js
@@ -4,32 +4,33 @@ import Carousel from 'react-bootstrap/Carousel';
   
 export default function App() {
   return (
-    <div style={{ display: 'block', width: 700, padding: 30 }}>
-      <h4>React-Bootstrap Carousel Component</h4>
-      <Carousel>
-        <Carousel.Item interval={1500}>
-          <img
-            className="d-block w-100"
-src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-            alt="Image One"
-          />
-          <Carousel.Caption>
-            <h3>Label for first slide</h3>
-            <p>Sample Text for Image One</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img
-            className="d-block w-100"
-src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-            alt="Image Two"
-          />
-          <Carousel.Caption>
-            <h3>Label for second slide</h3>
-            <p>Sample Text for Image Two</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
+    <div>carousel</div>
+//     <div style={{ display: 'block', width: 700, padding: 30 }}>
+//       <h4>React-Bootstrap Carousel Component</h4>
+//       <Carousel>
+//         <Carousel.Item interval={1500}>
+//           <img
+//             className="d-block w-100"
+// src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+//             alt="Image One"
+//           />
+//           <Carousel.Caption>
+//             <h3>Label for first slide</h3>
+//             <p>Sample Text for Image One</p>
+//           </Carousel.Caption>
+//         </Carousel.Item>
+//         <Carousel.Item interval={500}>
+//           <img
+//             className="d-block w-100"
+// src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+//             alt="Image Two"
+//           />
+//           <Carousel.Caption>
+//             <h3>Label for second slide</h3>
+//             <p>Sample Text for Image Two</p>
+//           </Carousel.Caption>
+//         </Carousel.Item>
+//       </Carousel>
+//     </div>
   );
 }
